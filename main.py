83761c6da32aa88bf55529bdc64fbf7e0dddd571
Markus Jalonen <majalone@tktk.ee>
2025-09-23 22:27:06 +0300

    add week 2 files

---
+++ main.py
@@ -0,0 +1,35 @@
+""" day = input("Mis päev on homme? (tööpäev/puhkepäev):")
+if day == "tööpäev":
+    print("Ma lähen magama, head ööd!")
+elif day == "puhkepäev":
+    print("Veel üks osa Netflixist")
+else:
+    print("Vale väärtus")
+     """
+
+""" 
+
+print("Tere tulemast programmi 'Finantsnõustaja'!")
+print("Sinu isiklik nõustaja ei tee emotsionaalseid oste.")
+money  = int(input("Kui palju raha sul praegu on?"))
+if money < 2500:
+    print ("Sul pole piisavalt raha. Ole kannatlik ja kogu edasi!")
+elif money == 2500:
+    print("Palju õnne, saad osta uue iphone 17 pro sularahas!")
+else: 
+    print("Saad osta iphone 17 pro ja veel raha jääb üle.")
+ """
+
+goal = 10000
+steps = int(input("mitu sammu oled teinud?:"))
+
+percent = (steps/goal) * 100
+print(f"{percent}%")
+if percent < 50:
+    print ("Alles poolel teel, liigu edasi!")
+elif percent < 75:
+    print(" Tubli, oled peaaegu kohal!")
+elif print < 100:
+    print("Suurepärane, oled peaaegu kohal!")
+else:
+    print("Palju õnne, oled oma eesmärgi täitnud!")
